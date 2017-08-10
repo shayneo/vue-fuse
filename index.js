73af105b59dd920b9042733d4b07b49e4f9f66ea
shayneosullivan <shayneosull@gmail.com>
2017-08-09 22:06:22 -0500

    kill arrow function to fix uglify bug

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ import fuse from 'fuse.js'
 
 VueFuse.install = function(Vue) {
   Vue.component('vue-fuse', VueFuse)
-  Vue.prototype.$testfuse = function (val) {
-    console.log(val)
-  }
+
   Vue.prototype.$search = function (term, list, options) {
-    return new Promise((resolve, reject) => {
+    return new Promise(function (resolve, reject) {
       var run = new fuse(list, options)
       var results = run.search(term)
       resolve(results)
