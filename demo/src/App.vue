471299360ebe01f6939ddf19dc5937329f032291
shayneosullivan <shayneosull@gmail.com>
2017-08-09 21:33:55 -0500

    1.2.0 add $search method

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div id="app">
-    <h1>vue-fuse demo</h1>
-    <p>This pluggin provides the 'vue-fuse' component, which can be dropped into your app. The component UI is a barebones &ltinput&gt tag, as shown below.</p><p> The component also takes a number of props, that are used to configure the search and define the event that is eminated when the search results update.</p>
-    <vue-fuse :keys="keys" :list="bikes" event-name="bikesChanged" :default-all="defaultAllToggle"></vue-fuse>
-    <label><input type="checkbox" @click="toggle">defaultAll</label>
-    <div id="list">
-      <ul>
-        <li v-for="bike in results" key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
-      </ul>
+    <div class="demo">
+      <h2><code>vue-fuse</code> component demo</h2>
+      <p>This pluggin provides the 'vue-fuse' component, which can be dropped into your app. The component UI is a barebones &ltinput&gt tag, as shown below.</p><p> The component also takes a number of props, that are used to configure the search and define the event that is eminated when the search results update.</p>
+      <vue-fuse :keys="keys" :list="bikes" event-name="bikesChanged" :default-all="defaultAllToggle"></vue-fuse>
+      <label><input type="checkbox" @click="toggle">defaultAll</label>
+      <div id="list">
+        <ul>
+          <li v-for="bike in componentResults" key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
+        </ul>
+      </div>
+    </div>
+    <hr>
+    <div class="demo">
+      <h2><code>$search</code> instance method demo</h2>
+      <p>Instead of using the component, you can use the $search instance method to execute a search programatically</p>
+      <input type="search" v-model="term">
+      <div id="list">
+        <ul>
+          <li v-for="bike in methodResults" key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
+        </ul>
+      </div>
+    </div>
+    <hr>
+    <div class="demo">
+      <h2>here's a list of bikes to search through!</h2>
+      <div id="list">
+        <ul>
+          <li v-for="bike in bikes" key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -18,8 +40,13 @@ export default {
   name: 'app',
   data () {
     return {
+      term: '',
       defaultAllToggle: false,
-      results: [],
+      options: {
+        keys: ["brand", "model.name", "model.id"]
+      },
+      componentResults: [],
+      methodResults: [],
       keys: ["brand", "model.name", "model.id"],
       bikes: [
         {
@@ -49,6 +76,34 @@ export default {
             name: "CRMO",
             id: "0432"
           }
+        },
+        {
+          brand: "Mongoose",
+          model: {
+            name: "isLoose",
+            id: "5000"
+          }
+        },
+        {
+          brand: "GT",
+          model: {
+            name: "Dyno",
+            id: "2345"
+          }
+        },
+        {
+          brand: "Canonndale",
+          model: {
+            name: "3500",
+            id: "cd35"
+          }
+        },
+        {
+          brand: "Giant",
+          model: {
+            name: "pegasus",
+            id: "6346"
+          }
         }
       ]
     }
@@ -59,25 +114,17 @@ export default {
       this.$forceUpdate()
     }
   },
+  watch: {
+    term () {
+      this.$search(this.term, this.bikes, this.options).then(results => {
+        this.methodResults = results
+      })
+    }
+  },
   created () {
     this.$on('bikesChanged', results => {
-      this.results = results
-    });
+      this.componentResults = results
+    })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-#list {
-  margin-left: 33%;
-  margin-right: 33%;
-}
-</style>
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import VueFuse from './src/VueFuse.vue'
+import fuse from 'fuse.js'
 
 VueFuse.install = function(Vue) {
   Vue.component('vue-fuse', VueFuse)
+  Vue.prototype.$testfuse = function (val) {
+    console.log(val)
+  }
+  Vue.prototype.$search = function (term, list, options) {
+    return new Promise((resolve, reject) => {
+      var run = new fuse(list, options)
+      var results = run.search(term)
+      resolve(results)
+    })
+  }
 }
 
 export default VueFuse
