6e36051b741a6e429b927a5967916cda3461e201
shayneosullivan <shayneosull@gmail.com>
2018-01-07 14:12:23 -0600

    update demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -3,11 +3,14 @@
     <div class="demo">
       <h2><code>vue-fuse</code> component demo</h2>
       <p>This pluggin provides the 'vue-fuse' component, which can be dropped into your app. The component UI is a barebones &ltinput&gt tag, as shown below.</p><p> The component also takes a number of props, that are used to configure the search and define the event that is eminated when the search results update.</p>
-      <vue-fuse :keys="keys" :list="bikes" event-name="bikesChanged" inputChangeEventName="bikeSearchInputChanged" :default-all="defaultAllToggle"></vue-fuse>
+      <vue-fuse :keys="keys" :list="bikes" event-name="bikesChanged" inputChangeEventName="bikeSearchInputChanged" :default-all="defaultAllToggle" :search="termtwo"></vue-fuse>
       <label><input type="checkbox" @click="toggle">defaultAll</label>
+      <br/>
+      <p> you can bind to the :search prop as well, so that you can map your own data source to the component. (try typing in the input here -->)
+      <input v-model="termtwo"/>
       <div id="list">
         <ul>
-          <li v-for="bike in componentResults" key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
+          <li v-for="bike in componentResults" :key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
         </ul>
       </div>
     </div>
@@ -18,7 +21,7 @@
       <input type="search" v-model="term">
       <div id="list">
         <ul>
-          <li v-for="bike in methodResults" key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
+          <li v-for="bike in methodResults" :key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
         </ul>
       </div>
     </div>
@@ -27,7 +30,7 @@
       <h2>here's a list of bikes to search through!</h2>
       <div id="list">
         <ul>
-          <li v-for="bike in bikes" key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
+          <li v-for="bike in bikes" :key="bike.model.id">{{bike.brand}} | {{bike.model.name}} {{bike.model.id}}</li>
         </ul>
       </div>
     </div>
@@ -41,6 +44,7 @@ export default {
   data () {
     return {
       term: '',
+      termtwo: '',
       defaultAllToggle: false,
       options: {
         keys: ["brand", "model.name", "model.id"]
@@ -53,49 +57,49 @@ export default {
           brand: "Schwinn",
           model: {
             name: "Classic",
-            id: "1345"
+            id: "1345ppppgd"
           }
         },
         {
           brand: "Red Line",
           model: {
             name: "Flight",
-            id: "5430"
+            id: "5430sdfsdf"
           }
         },
         {
           brand: "Hoffman",
           model: {
             name: "Condore",
-            id: "0543"
+            id: "0543ADS"
           }
         },
         {
           brand: "Tribe",
           model: {
             name: "CRMO",
-            id: "0432"
+            id: "043dfgdfg2"
           }
         },
         {
           brand: "Mongoose",
           model: {
             name: "isLoose",
-            id: "5000"
+            id: "50ff00"
           }
         },
         {
           brand: "GT",
           model: {
             name: "Dyno",
-            id: "2345"
+            id: "234fff5"
           }
         },
         {
           brand: "Canonndale",
           model: {
             name: "3500",
-            id: "cd35"
+            id: "cd3asd5"
           }
         },
         {
