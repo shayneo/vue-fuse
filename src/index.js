382b923688f3d5654996a0de9e9ec36412f52c9f
Shayne O'Sullivan <shayne@vin.li>
2019-05-08 21:24:39 -0500

    Expose install method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
 import VueFuse from './components/VueFuse.vue'
 import Fuse from 'fuse.js'
 
-Vue.component(VueFuse.name, VueFuse)
-Vue.prototype.$search = function (term, list, options) {
-  return new Promise(function (resolve, reject) {
-    var run = new Fuse(list, options)
-    var results = run.search(term)
-    resolve(results)
-  })
+function install(Vue) {
+  Vue.component(VueFuse.name, VueFuse)
+  Vue.prototype.$search = function (term, list, options) {
+    return new Promise(function (resolve, reject) {
+      var run = new Fuse(list, options)
+      var results = run.search(term)
+      resolve(results)
+    })
+  }
 }
-export default { VueFuse }
+
+export default install
