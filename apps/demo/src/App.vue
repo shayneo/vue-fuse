d25aa980488f8cf93b06a753c0d7d6d18906fd40
Shayne O'Sullivan <shayneosull@gmail.com>
2021-10-09 14:46:24 -0500

    Save place

--- apps/demo/src/App.vue
+++ apps/demo/src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>hello world</div>
-  <input type="text" v-model="fuse.term.value">
-  {{fuse.results.value }}
+  <h1>VueFuse Example</h1>
+  <input type="text" v-model="search">
+  <div v-for="(book, i) in results" :key="i">
+    <div>{{book.name}}</div>
+    <div>{{book.description}}</div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent,ref, watchEffect  } from "vue";
+import { defineComponent } from "vue";
 import { useVueFuse } from 'vue-fuse'
+import { books } from './books'
 
 export default defineComponent({
   name: 'App',
   setup () {
-    const fuse = useVueFuse(['aaa', 'bbb', 'ccc'])
+    const fuse = useVueFuse(books, { keys: [{ name: 'name', weight: 2} , { name: 'description', weight: 1}]})
+    const { search, results } = fuse
     return {
-      fuse,
+      search,
+      results
     }
   }
 })
 </script>
+
 
--- apps/vue-fuse/use-fuse.ts
+++ apps/vue-fuse/use-fuse.ts
@@ -1,37 +1,42 @@
 import Fuse from 'fuse.js'
-import { computed, watch, ref, Ref, ComputedRef, toRefs , ToRefs} from 'vue-demi'
+import { computed, watch, ref, Ref, ComputedRef } from 'vue-demi'
 
 export class VueFuse<T> {
   fuse: Ref<Fuse<T>>
-  results: Ref<Fuse.FuseResult<T>[]>
-  term: Ref<string>
+  resultsRaw: Ref<Fuse.FuseResult<T>[]>
+  results: ComputedRef<Array<T>>
+  search: Ref<string>
   noResults: ComputedRef<boolean>
-  search: (search?: string | null) => void
+  runSearch: (search?: string | null) => void
   constructor(list: readonly T[], options?: Fuse.IFuseOptions<T>) {
     this.fuse = ref(new Fuse(list, options))
-    this.results = ref([])
-    this.term = ref('')
+    this.resultsRaw = ref([])
+    this.search = ref('')
+
     this.noResults = computed(() => {
-      if (this.results.value.length === 0 && this.term.value.length > 0) {
+      if (this.results.value.length === 0 && this.search.value.length > 0) {
         return true
       }
       return false
     })
-    this.search = (search?: string | null) => {
+
+    this.results = computed(() => this.resultsRaw.value.map(r => r.item))
+
+    this.runSearch = (search?: string | null) => {
       if (!this.fuse) {
         return
       }
-      if (!this.search) {
-        this.results.value = []
+      if (!search) {
+        this.resultsRaw.value = []
         return
       }
-      this.results.value = this.fuse.value.search(search as string)
+      this.resultsRaw.value = this.fuse.value.search(search as string)
     }
-    watch(this.term, this.search)
+    watch(this.search, this.runSearch)
   }
 }
 
-export function useVueFuse<T>(list: readonly T[], options?: Fuse.IFuseOptions<T>): ToRefs<VueFuse<T>> {
- const instance = new VueFuse(list, options)
- return toRefs(instance)
+export function useVueFuse<T>(list: readonly T[], options?: Fuse.IFuseOptions<T>): VueFuse<T> {
+ return new VueFuse(list, options)
 }
+
