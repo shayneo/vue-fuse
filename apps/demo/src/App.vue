bb1e033c0c468b44a089acf8a01175731ba9444a
Shayne O'Sullivan <shayneosull@gmail.com>
2021-10-09 14:48:32 -0500

    Show no results

--- apps/demo/src/App.vue
+++ apps/demo/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>VueFuse Example</h1>
   <input type="text" v-model="search">
+  <p v-if="noResults">Sorry, no results for {{search}}</p>
   <div v-for="(book, i) in results" :key="i">
     <div>{{book.name}}</div>
     <div>{{book.description}}</div>
@@ -16,10 +17,11 @@ export default defineComponent({
   name: 'App',
   setup () {
     const fuse = useVueFuse(books, { keys: [{ name: 'name', weight: 2} , { name: 'description', weight: 1}]})
-    const { search, results } = fuse
+    const { search, results, noResults } = fuse
     return {
       search,
-      results
+      results,
+      noResults,
     }
   }
 })
